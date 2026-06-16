772491d319efd6fb241e5c0547d227579cdb6e49
al-farabi <farabisyl2017@gmail.com>
2026-06-16 13:27:35 +0600

    user can login successfully

--- src/Pages/Auth/Login.jsx
+++ src/Pages/Auth/Login.jsx
@@ -1,21 +1,36 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import useAuth from '../../../Hooks/useAuth'
 
 function Login() {
+
+    const { register, handleSubmit } = useForm()
+    const { signInUser } = useAuth()
+    const handleLogin = (data) => {
+        signInUser(data.email, data.password)
+            .then(result => {
+                console.log(result.user)
+            })
+            .catch(err => {
+                console.log(err.message)
+            })
+    }
+
     return (
         <div>
             <div className="hero bg-base-200 min-h-screen">
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-                        <div className="card-body">
+                        <form onSubmit={handleSubmit(handleLogin)} className="card-body">
                             <fieldset className="fieldset">
                                 <label className="label">Email</label>
-                                <input type="email" className="input" placeholder="Email" />
+                                <input type="email" {...register("email")} className="input" placeholder="Email" />
                                 <label className="label">Password</label>
-                                <input type="password" className="input" placeholder="Password" />
+                                <input type="password" {...register("password")} className="input" placeholder="Password" />
                                 <div><a className="link link-hover">Forgot password?</a></div>
                                 <button className="btn btn-neutral mt-4">Login</button>
                             </fieldset>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
